1798be725833a8fe5de7c13822f38913d0e2d577
phreak <traik@polarisedkids.com>
2014-08-29 15:10:15 +0200

    Added some text changes

--- views/settings.php
+++ views/settings.php
@@ -6,8 +6,8 @@ echo Wrap($this->Data('Title'), 'h1');
 echo $this->Form->Open();
 echo $this->Form->Errors();
 
-echo Wrap(Wrap(T('Plugin.DiscussionInserts.Settings.Desc'), 'div'), 'div', array('class' => 'Wrap'));
-echo Wrap(Anchor(T('Plugin.DiscussionInserts.Add'), 'settings/discussioninserts/add', array('class' => 'SmallButton')), 'div', array('class' => 'Wrap'));
+echo Wrap(Wrap(T('Discussion Inserts allows you to add custom text or HTML between the starting post and the first answer of a discussion.'), 'div'), 'div', array('class' => 'Wrap'));
+echo Wrap(Anchor(T('Add Insert'), 'settings/discussioninserts/add', array('class' => 'SmallButton')), 'div', array('class' => 'Wrap'));
 
 ?>
 <table id="Actions" class="AltRows">
@@ -15,8 +15,8 @@ echo Wrap(Anchor(T('Plugin.DiscussionInserts.Add'), 'settings/discussioninserts/
     <tr>
       <th><?php echo T('Name'); ?></th>
       <th><?php echo T('Discussion'); ?></th>
-      <th><?php echo T('Body'); ?></th>
-      <th><?php echo T('Mobile Body'); ?></th>
+      <th><?php echo T('Insert'); ?></th>
+      <th><?php echo T('Mobile Insert'); ?></th>
       <th><?php echo T('Options'); ?></th>
     </tr>
   </thead>
